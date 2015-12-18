6eb7b0f2a2c47156ebbcf9d26dbc5d96595b3a91
fengyifei <617218557@qq.com>
2015-12-18 17:36:07 +0800

    测试1

--- app/src/main/java/com/fyf/parkinglot/activity/myFriends/MyFriendsActivity.java
+++ app/src/main/java/com/fyf/parkinglot/activity/myFriends/MyFriendsActivity.java
@@ -42,6 +42,7 @@ public class MyFriendsActivity extends AppCompatActivity {
         tv_title = (TextView) findViewById(R.id.layout_actionBar_tv_title);
         btn_next = (Button) findViewById(R.id.layout_actionBar_btn_next);
 
+
         lv_friends = (JazzyListView) findViewById(R.id.activity_my_friends_lv_friends);
     }
 
