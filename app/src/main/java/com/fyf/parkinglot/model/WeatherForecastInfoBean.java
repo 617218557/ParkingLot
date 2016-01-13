a89ac262d9289e013804263661697eff3a7ad28e
fengyifei <617218557@qq.com>
2016-01-13 16:23:40 +0800

    优化不止

--- app/src/main/java/com/fyf/parkinglot/model/WeatherForecastInfoBean.java
+++ app/src/main/java/com/fyf/parkinglot/model/WeatherForecastInfoBean.java
@@ -6,7 +6,211 @@ import java.util.List;
  * Created by fengyifei on 15/12/18.
  */
 public class WeatherForecastInfoBean {
+    /**
+     * error : 0
+     * status : success
+     * date : 2015-12-18
+     * results : [{"currentCity":"西安","pm25":"105","index":[{"title":"穿衣","zs":"冷","tipt":"穿衣指数","des":"天气冷，建议着棉服、羽绒服、皮夹克加羊毛衫等冬季服装。年老体弱者宜着厚棉衣、冬大衣或厚羽绒服。"},{"title":"洗车","zs":"较适宜","tipt":"洗车指数","des":"较适宜洗车，未来一天无雨，风力较小，擦洗一新的汽车至少能保持一天。"},{"title":"旅游","zs":"适宜","tipt":"旅游指数","des":"天气较好，气温稍低，会感觉稍微有点凉，不过也是个好天气哦。适宜旅游，可不要错过机会呦！"},{"title":"感冒","zs":"较易发","tipt":"感冒指数","des":"天气较凉，较易发生感冒，请适当增加衣服。体质较弱的朋友尤其应该注意防护。"},{"title":"运动","zs":"较适宜","tipt":"运动指数","des":"阴天，较适宜进行各种户内外运动。"},{"title":"紫外线强度","zs":"最弱","tipt":"紫外线强度指数","des":"属弱紫外线辐射天气，无需特别防护。若长期在户外，建议涂擦SPF在8-12之间的防晒护肤品。"}],"weather_data":[{"date":"周五 12月18日 (实时：3℃)","dayPictureUrl":"http://api.map.baidu.com/images/weather/day/yin.png","nightPictureUrl":"http://api.map.baidu.com/images/weather/night/yin.png","weather":"阴","wind":"东风微风","temperature":"5 ~ -3℃"},{"date":"周六","dayPictureUrl":"http://api.map.baidu.com/images/weather/day/yin.png","nightPictureUrl":"http://api.map.baidu.com/images/weather/night/duoyun.png","weather":"阴转多云","wind":"东风微风","temperature":"4 ~ -3℃"},{"date":"周日","dayPictureUrl":"http://api.map.baidu.com/images/weather/day/qing.png","nightPictureUrl":"http://api.map.baidu.com/images/weather/night/yin.png","weather":"晴转阴","wind":"东风微风","temperature":"6 ~ -3℃"},{"date":"周一","dayPictureUrl":"http://api.map.baidu.com/images/weather/day/duoyun.png","nightPictureUrl":"http://api.map.baidu.com/images/weather/night/duoyun.png","weather":"多云","wind":"东风微风","temperature":"4 ~ -1℃"}]}]
+     */
 
+    private int error;
+    private String status;
+    private String date;
+    /**
+     * currentCity : 西安
+     * pm25 : 105
+     * index : [{"title":"穿衣","zs":"冷","tipt":"穿衣指数","des":"天气冷，建议着棉服、羽绒服、皮夹克加羊毛衫等冬季服装。年老体弱者宜着厚棉衣、冬大衣或厚羽绒服。"},{"title":"洗车","zs":"较适宜","tipt":"洗车指数","des":"较适宜洗车，未来一天无雨，风力较小，擦洗一新的汽车至少能保持一天。"},{"title":"旅游","zs":"适宜","tipt":"旅游指数","des":"天气较好，气温稍低，会感觉稍微有点凉，不过也是个好天气哦。适宜旅游，可不要错过机会呦！"},{"title":"感冒","zs":"较易发","tipt":"感冒指数","des":"天气较凉，较易发生感冒，请适当增加衣服。体质较弱的朋友尤其应该注意防护。"},{"title":"运动","zs":"较适宜","tipt":"运动指数","des":"阴天，较适宜进行各种户内外运动。"},{"title":"紫外线强度","zs":"最弱","tipt":"紫外线强度指数","des":"属弱紫外线辐射天气，无需特别防护。若长期在户外，建议涂擦SPF在8-12之间的防晒护肤品。"}]
+     * weather_data : [{"date":"周五 12月18日 (实时：3℃)","dayPictureUrl":"http://api.map.baidu.com/images/weather/day/yin.png","nightPictureUrl":"http://api.map.baidu.com/images/weather/night/yin.png","weather":"阴","wind":"东风微风","temperature":"5 ~ -3℃"},{"date":"周六","dayPictureUrl":"http://api.map.baidu.com/images/weather/day/yin.png","nightPictureUrl":"http://api.map.baidu.com/images/weather/night/duoyun.png","weather":"阴转多云","wind":"东风微风","temperature":"4 ~ -3℃"},{"date":"周日","dayPictureUrl":"http://api.map.baidu.com/images/weather/day/qing.png","nightPictureUrl":"http://api.map.baidu.com/images/weather/night/yin.png","weather":"晴转阴","wind":"东风微风","temperature":"6 ~ -3℃"},{"date":"周一","dayPictureUrl":"http://api.map.baidu.com/images/weather/day/duoyun.png","nightPictureUrl":"http://api.map.baidu.com/images/weather/night/duoyun.png","weather":"多云","wind":"东风微风","temperature":"4 ~ -1℃"}]
+     */
+
+    private List<ResultsEntity> results;
+
+    public void setError(int error) {
+        this.error = error;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setResults(List<ResultsEntity> results) {
+        this.results = results;
+    }
+
+    public int getError() {
+        return error;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public List<ResultsEntity> getResults() {
+        return results;
+    }
+
+    public static class ResultsEntity {
+        private String currentCity;
+        private String pm25;
+        /**
+         * title : 穿衣
+         * zs : 冷
+         * tipt : 穿衣指数
+         * des : 天气冷，建议着棉服、羽绒服、皮夹克加羊毛衫等冬季服装。年老体弱者宜着厚棉衣、冬大衣或厚羽绒服。
+         */
+
+        private List<IndexEntity> index;
+        /**
+         * date : 周五 12月18日 (实时：3℃)
+         * dayPictureUrl : http://api.map.baidu.com/images/weather/day/yin.png
+         * nightPictureUrl : http://api.map.baidu.com/images/weather/night/yin.png
+         * weather : 阴
+         * wind : 东风微风
+         * temperature : 5 ~ -3℃
+         */
+
+        private List<WeatherDataEntity> weather_data;
+
+        public void setCurrentCity(String currentCity) {
+            this.currentCity = currentCity;
+        }
+
+        public void setPm25(String pm25) {
+            this.pm25 = pm25;
+        }
+
+        public void setIndex(List<IndexEntity> index) {
+            this.index = index;
+        }
+
+        public void setWeather_data(List<WeatherDataEntity> weather_data) {
+            this.weather_data = weather_data;
+        }
+
+        public String getCurrentCity() {
+            return currentCity;
+        }
+
+        public String getPm25() {
+            return pm25;
+        }
+
+        public List<IndexEntity> getIndex() {
+            return index;
+        }
+
+        public List<WeatherDataEntity> getWeather_data() {
+            return weather_data;
+        }
+
+        public static class IndexEntity {
+            private String title;
+            private String zs;
+            private String tipt;
+            private String des;
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public void setZs(String zs) {
+                this.zs = zs;
+            }
+
+            public void setTipt(String tipt) {
+                this.tipt = tipt;
+            }
+
+            public void setDes(String des) {
+                this.des = des;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public String getZs() {
+                return zs;
+            }
+
+            public String getTipt() {
+                return tipt;
+            }
+
+            public String getDes() {
+                return des;
+            }
+        }
+
+        public static class WeatherDataEntity {
+            private String date;
+            private String dayPictureUrl;
+            private String nightPictureUrl;
+            private String weather;
+            private String wind;
+            private String temperature;
+
+            public void setDate(String date) {
+                this.date = date;
+            }
+
+            public void setDayPictureUrl(String dayPictureUrl) {
+                this.dayPictureUrl = dayPictureUrl;
+            }
+
+            public void setNightPictureUrl(String nightPictureUrl) {
+                this.nightPictureUrl = nightPictureUrl;
+            }
+
+            public void setWeather(String weather) {
+                this.weather = weather;
+            }
+
+            public void setWind(String wind) {
+                this.wind = wind;
+            }
+
+            public void setTemperature(String temperature) {
+                this.temperature = temperature;
+            }
+
+            public String getDate() {
+                return date;
+            }
+
+            public String getDayPictureUrl() {
+                return dayPictureUrl;
+            }
+
+            public String getNightPictureUrl() {
+                return nightPictureUrl;
+            }
+
+            public String getWeather() {
+                return weather;
+            }
+
+            public String getWind() {
+                return wind;
+            }
+
+            public String getTemperature() {
+                return temperature;
+            }
+        }
+    }
+
+
+
+   /*
     private int error;
     private String status;
     private String date;
---
+++ app/src/main/java/com/fyf/parkinglot/view/HaloView1.java
@@ -0,0 +1,171 @@
+package com.fyf.parkinglot.view;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.SystemClock;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.fyf.parkinglot.R;
+import com.fyf.parkinglot.common.ScreenInfo;
+
+import java.util.Random;
+
+public class HaloView1 extends View implements Runnable {
+
+    @Override
+    public void run() {
+        loadFlower();
+        addRect();
+        while (true) {
+            inva();
+            SystemClock.sleep(85);
+        }
+    }
+
+    Bitmap mHalo = null;
+    MyHalo myHalo[] = new MyHalo[12];
+    private Integer[] offsetX;// X轴每次移动像素
+    private Integer[] offsetY;// Y轴每次移动像素
+    Random r = new Random();
+    Matrix m = new Matrix();
+    Paint p = new Paint();
+
+    int mW = ScreenInfo.getScreenWidth();// 屏幕宽度
+    int mH = ScreenInfo.getScreenHeight();// 屏幕高度
+    // 像素密度
+    float de = ScreenInfo.getDe();
+
+    public void setOffset() {
+        offsetX = new Integer[]{(int) (int) (1 * de), (int) (2 * de),};
+        offsetY = new Integer[]{(int) (1 * de), (int) (2 * de),};
+    }
+
+    public HaloView1(Context context) {
+        super(context);
+        setOffset();
+    }
+
+    public HaloView1(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setOffset();
+    }
+
+    public HaloView1(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOffset();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < myHalo.length; i++) {
+            MyHalo myHaloItem = myHalo[i];
+
+            int t = myHaloItem.t;
+            t--;
+            if (t <= 0) {
+
+                if (myHaloItem.flagX == 0) {
+                    myHaloItem.x -= myHaloItem.speedX;
+                    if (myHaloItem.x < -mW * 0.5)
+                        myHaloItem.flagX = 1;
+                } else {
+                    myHaloItem.x += myHaloItem.speedX;
+                    if (myHaloItem.x > mW + mW * 0.5)
+                        myHaloItem.flagX = 0;
+                }
+                if (myHaloItem.flagY == 0) {
+                    myHaloItem.y -= myHaloItem.speedY;
+                    if (myHaloItem.y < -mH * 0.5)
+                        myHaloItem.flagY = 1;
+                } else {
+                    myHaloItem.y += myHaloItem.speedY;
+                    if (myHaloItem.y > mH + mH * 0.5)
+                        myHaloItem.flagY = 0;
+                }
+
+                canvas.save();
+                m.reset();
+                m.setScale(myHaloItem.s, myHaloItem.s);
+                canvas.setMatrix(m);
+                p.setAlpha(myHaloItem.alpha);
+                canvas.drawBitmap(mHalo, myHaloItem.x, myHaloItem.y, p);
+                canvas.restore();
+            }
+            // 当t过小时，重置t
+            if (t == -10000)
+                t = -1;
+            myHaloItem.t = t;
+        }
+        super.onDraw(canvas);
+    }
+
+    public void loadFlower() {
+        Resources r = this.getContext().getResources();
+        mHalo = ((BitmapDrawable) r.getDrawable(R.drawable.ic_fog_circle))
+                .getBitmap();
+    }
+
+    public void recly() {
+        if (mHalo != null && !mHalo.isRecycled()) {
+            mHalo.recycle();
+        }
+    }
+
+    public void addRect() {
+        for (int i = 0; i < myHalo.length; i++) {
+            myHalo[i] = new MyHalo();
+        }
+    }
+
+    public void inva() {
+        invalidate();
+    }
+
+    class MyHalo {
+        int x;// 距离屏幕左边距离
+        int y;// 距离屏幕右边距离
+        float s;// 放大倍数
+        int alpha;// 透明度
+        int t;// 光晕开始运动时间延迟
+        int speedX;// X轴动画速度（每次移动的像素）
+        int speedY;// Y轴动画速度（每次移动的像素）
+
+        int flagX, flagY;// 记录X,Y轴移动方向(0为向上向左，1为向下向右)
+
+        public void init() {
+
+            this.x = r.nextInt(mW);
+            this.y = r.nextInt(mH);
+
+            this.s = r.nextFloat();
+            // 防止过小
+            if (this.s < 0.1f)
+                s += 0.2f;
+                // 防止过大
+            else if (this.s > 0.5f)
+                s -= 0.2f;
+
+            this.alpha = r.nextInt(155) + 100;
+            this.t = r.nextInt(80);
+
+            this.speedX = offsetX[r.nextInt(offsetX.length)];
+            this.speedY = offsetY[r.nextInt(offsetY.length)];
+            // 初始化移动方向
+            this.flagX = r.nextInt(2);
+            this.flagY = r.nextInt(2);
+        }
+
+        public MyHalo() {
+            super();
+            init();
+        }
+
+    }
+}
